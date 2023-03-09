f17266be4aaf4ecbde1420543af09a63f620d168
helenhe40 <yhe@lbl.gov>
2023-03-09 12:14:06 -0800

    update run-xthi-pm.sh

--- CPU/run-xthi-pm.sh
+++ CPU/run-xthi-pm.sh
@@ -9,4 +9,14 @@ export OMP_NUM_THREADS=8
 export OMP_PROC_BIND=spread
 export OMP_PLACES=threads
 
+echo "== run 1 ===="
 srun -n 8 -c 64 --cpu-bind=cores ./xthi
+
+echo "== run 2, sorted ===="
+srun -n 8 -c 64 --cpu-bind=cores ./xthi |sort -k4n,6n
+
+
+echo "====run 3, with display_affinity============="
+export OMP_DISPLAY_AFFINITY=true
+export OMP_AFFINITY_FORMAT="host=%H, pid=%P, thread_num=%n, thread affinity=%A"
+srun -n 8 -c 64 --cpu-bind=cores ./xthi |sort -k4n,6n
